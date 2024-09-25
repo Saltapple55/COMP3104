666f0ce2ad648278134171029939c33594d8fcc0
Saltapple55 <tjandraaudrey@hotmail.com>
2024-09-25 19:58:38 -0400

    edited github actions to have steps and runs

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello world from devops")
+/*
+
+language: node_js
+node_js:
+- "stable"
+deploy:
+provider: pages
+skip_cleanup: true
+github_token: $github_token
+local_dir: build
+on:
+branch: master
+`*/ 
